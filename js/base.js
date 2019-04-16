d2a9203c09488e79c729db3ee35866ed16ab28b6
Matt Wujek <mattwujek@Loaner-MacBook-Pro-2.local>
2019-04-16 09:16:33 -0700

    desktop draft finished

--- js/base.js
+++ js/base.js
@@ -8,29 +8,58 @@ jQuery( document ).ready( function() {
     prev = 0;
     nav = $('header#asd');
     scroller = $('.arrow-container');
+
+
+    // lightbox plugin
     $('[data-fancybox="gallery"]').fancybox({
-      // Options will go here
-      //infobar: false,
-      //toolbar: false,
-      //arrows: false
       buttons: [
-        //"zoom",
-        //"share",
-        //"slideShow",
-        //"fullScreen",
-        //"download",
-        //"thumbs",
         "close"
       ],
     });
+
+
 
   $('.flickity-slideshow').flickity({
     // options
     cellAlign: 'left',
     contain: true,
-    prevNextButtons: false,
+    //prevNextButtons: false,
     adaptiveHeight: true
   });
+
+
+  var initAccordion = function(){
+    var accordion = $('.accordion-container');
+    accordion.each(function(){
+      var items = $(this).find('.accordion-item');
+      items.each(function(){
+        $(this).find('.accordion-content').slideUp(0);
+      });
+    });
+  }
+
+  var updateAccordion = function(itemClicked){
+    console.log('aaaa');
+    if(itemClicked.hasClass('active')){
+      itemClicked.find('.accordion-content').slideUp();
+      //itemClicked.removeClass('active');
+    } else {
+      itemClicked.find('.accordion-content').slideDown();
+      //itemClicked.addClass('active');
+    }
+    itemClicked.toggleClass('active');
+  }
+
+  // if accordion present
+  
+  if($('.accordion-container').length !== 0){
+    initAccordion();  
+    $('.accordion-item').click(function(){
+      var el = $(this);
+      updateAccordion(el);
+    })
+  }
+  
 
 }); // end of ready
 
